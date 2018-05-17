43a26f13c1c4f5d83e942b7a11ad06a9b8bf4a5d
taryn yaeger <tsyaeger@gmail.com>
2018-05-17 20:10:20 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,4 +3,39 @@
 
 def key_for_min_value(name_hash)
 
+  smallest = []
+  
+  name_hash.collect do |k,v|
+  	puts "key: #{k}."
+  	puts "val: #{v}"
+  	
+    if smallest == [] 
+      smallest = [k,v]
+    else 
+
+      if smallest[1].to_i > v
+        smallest = [k,v]
+      end
+
+    end
+    puts smallest[0]
+
+  end
+enddef key_for_min_value(name_hash)
+
+  smallest = []
+  
+  name_hash.collect do |k,v|
+  	puts v
+  	puts k
+  	
+    if smallest == [] 
+      smallest = name_hash[k,v]
+    else 
+      if smallest[1].to_i > v
+        smallest = [k,v]
+      end
+    end
+    
+  end
 end
